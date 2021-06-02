b362d7498611aa5da147ff4f91336dd1e43feb32
salomaogithub <84052529+salomaogithub@users.noreply.github.com>
2021-06-01 21:27:14 -0300

    Add files via upload

---
+++ atividade_4/src/atividade/Vendedor.java
@@ -0,0 +1,88 @@
+package atividade;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vendedor extends Funcionario {
+	public Gerente gerente;
+    @SuppressWarnings("rawtypes")
+	protected List vendas = new ArrayList<>();
+    protected double totalVendas;
+    protected double comissaoVendedor;
+    protected double totalComissao;
+    
+    public Vendedor(int idade, String nome, String cpf, String cargo, double salario, Gerente gerente, double totalVendas, double comissaoVendedor, double totalComissao) {
+        super(idade, nome, cpf, cargo, salario);
+        this.gerente = gerente;
+        this.totalVendas = totalVendas;
+        this.comissaoVendedor = comissaoVendedor;
+        this.totalComissao = totalComissao;
+    }
+    
+    @SuppressWarnings("unchecked")
+	public List<Vendas> getVendas() {
+        return vendas;
+    }
+
+    public void setVendas(List<Vendas> vendas) {
+        this.vendas = vendas;
+    }
+
+    public double getTotalVendas() {
+        for(int i = 0 ;i < vendas.size(); i++){
+            totalVendas += vendas.get(i).hashCode();
+        }
+        return totalVendas;
+    }
+
+    public void setTotalVendas(double totalVendas) {
+        this.totalVendas = totalVendas;
+    }
+
+    public double getComissaoVendedor() {
+        return comissaoVendedor;
+    }
+
+    public void setComissaoVendedor(double comissaoVendedor) {
+        this.comissaoVendedor = comissaoVendedor;
+    }
+
+    public double getTotalComissao() {
+        totalComissao = totalVendas * comissaoVendedor;
+        return totalComissao;
+    }
+
+    public void setTotalComissao(double totalComissao) {
+        this.totalComissao = totalComissao;
+    }
+
+    public Gerente getGerente() {
+        return gerente;
+    }
+
+    public void setGerente(Gerente gerente) {
+        this.gerente = gerente;
+    }
+
+    @Override
+    public double getSalario() {
+        salario += totalComissao;
+        return salario;
+    }
+
+    @Override
+    public String toString() {
+        return "Vendedor{" +
+                "gerente=" + gerente +
+                ", vendas=" + vendas +
+                ", totalVendas=" + totalVendas +
+                ", comissaoVendedor=" + comissaoVendedor +
+                ", totalComissao=" + totalComissao +
+                "} " + super.toString();
+    }
+
+
+
+
+}
